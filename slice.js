dd55ef311411dc20b165a68f7bf0e76011d4e6a0
G MANIKANTA  ANJANEYA <gantamanikanta46@gmail.com>
2025-02-10 21:08:28 +0530

    slice.js

---
+++ slice.js
@@ -0,0 +1,13 @@
+// using of slice method
+var name=["mani","chaitu","adi","esu","sudhar"]
+var result1= name.slice(1)
+var result2= name.slice(1,3)
+console.log(result1)
+console.log(result2)
+
+
+// output
+
+// [ 'chaitu', 'adi', 'esu', 'sudhar' ]
+
+//[ 'chaitu', 'adi' ]
