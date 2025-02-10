5fcccc2487f7d36d088187b33276bf1687e84529
G MANIKANTA  ANJANEYA <gantamanikanta46@gmail.com>
2025-02-10 21:10:46 +0530

    splice.js

---
+++ splice.js
@@ -0,0 +1,11 @@
+//using of splice method
+var name=["mani","chaitu","adi","esu","sudhar"]
+//here 3 is count
+var result= name.splice(0,3,"hi")
+
+console.log(result)
+
+
+
+// output
+// [ 'mani', 'chaitu', 'adi' ]
